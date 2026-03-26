openfde/lineageos_android_frameworks_base

wm: Include task id in surface name for easier tracking
Change-Id: Ia64c3e8ef8e4514223649417d843d7ef65e2f5ba

services/core/java/com/android/server/wm/WindowStateAnimator.java
@@ -514,7 +514,13 @@ class WindowStateAnimator {
                 flags |= SurfaceControl.OPAQUE;
             }
 
-            mSurfaceController = new WindowSurfaceController(attrs.getTitle().toString(), width,
+            String name = attrs.getTitle().toString();
+            Task task = mWin.getTask();
+            if (task != null) {
+                name = String.format("TID:%d#", task.mTaskId) + name;
+            }
+
+            mSurfaceController = new WindowSurfaceController(name, width,
                     height, format, flags, this, windowType, ownerUid);
             mSurfaceController.setColorSpaceAgnostic((attrs.privateFlags
                     & WindowManager.LayoutParams.PRIVATE_FLAG_COLOR_SPACE_AGNOSTIC) != 0);

Mnauiinecek/android_frameworks_base

services/core/java/com/android/server/wm/WindowStateAnimator.java
@@ -309,7 +309,12 @@ class WindowStateAnimator {
             final boolean isHwAccelerated = (attrs.flags & FLAG_HARDWARE_ACCELERATED) != 0;
             final int format = isHwAccelerated ? PixelFormat.TRANSLUCENT : attrs.format;
 
-            mSurfaceController = new WindowSurfaceController(attrs.getTitle().toString(), format,
+            String name = attrs.getTitle().toString();
+            Task task = mWin.getTask();
+            if (task != null) {
+                name = String.format("TID:%d#", task.mTaskId) + name;
+            }
+            mSurfaceController = new WindowSurfaceController(name, format,
                     flags, this, attrs.type);
             mSurfaceController.setColorSpaceAgnostic((attrs.privateFlags
                     & WindowManager.LayoutParams.PRIVATE_FLAG_COLOR_SPACE_AGNOSTIC) != 0);