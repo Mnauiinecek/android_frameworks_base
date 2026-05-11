ffe8a562fd8c6d73484081caf964d35abfde435e
jingjiezhuang <jingjie.zhuang@igg.com>
2026-05-11 21:52:44 +0800

    clipboard: Talk with waydroid host service to get clipboard synchronized
    Co-authored-by: SupeChicken666 <me@supechicken666.dev>
    Change-Id: I1d46306ada5967e27d568284e477eb0c88150016

--- services/core/java/com/android/server/clipboard/ClipboardService.java
+++ services/core/java/com/android/server/clipboard/ClipboardService.java
@@ -120,6 +120,8 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
+import lineageos.waydroid.WaydroidClipboard;
+
 /**
  * Implementation of the clipboard for copy and paste.
  * <p>
@@ -172,6 +174,7 @@ public class ClipboardService extends SystemService {
     private final IBinder mPermissionOwner;
     private final Consumer<ClipData> mClipboardMonitor;
     private final Handler mWorkerHandler;
+    private WaydroidClipboard mWaydroidClipboard;
 
     @GuardedBy("mLock")
     // Maps (userId, deviceId) to Clipboard.
@@ -235,6 +238,7 @@ public class ClipboardService extends SystemService {
         HandlerThread workerThread = new HandlerThread(TAG);
         workerThread.start();
         mWorkerHandler = workerThread.getThreadHandler();
+        mWaydroidClipboard = WaydroidClipboard.getInstance(context);
     }
 
     @Override
@@ -896,6 +900,11 @@ public class ClipboardService extends SystemService {
             clipboard = new Clipboard(userId, deviceId);
             mClipboards.add(userId, deviceId, clipboard);
         }
+        if (mWaydroidClipboard != null && mWaydroidClipboard.getService() != null) {
+            clipboard.primaryClip = new ClipData("host clipboard",
+                                                 new String[]{"text/plain"},
+                                                 new ClipData.Item(mWaydroidClipboard.getClipboardData()));
+        }
         return clipboard;
     }
 
@@ -1032,6 +1041,10 @@ public class ClipboardService extends SystemService {
             final ClipDescription description = clip.getDescription();
             if (description != null) {
                 description.setTimestamp(System.currentTimeMillis());
+            }
+            if (mWaydroidClipboard != null) {
+                ClipData.Item firstItem = clip.getItemAt(0);
+                mWaydroidClipboard.sendClipboardData(firstItem.getText().toString());
             }
         }
         sendClipChangedBroadcast(clipboard);
